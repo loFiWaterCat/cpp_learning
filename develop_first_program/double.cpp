f66b198946bbd67d21fcb5a7c9e621565d4e4101
alan <alan001tk@gmail.com>
2023-01-10 23:10:46 -0800

    Add triple functionality to double program

--- develop_first_program/double.cpp
+++ develop_first_program/double.cpp
@@ -5,6 +5,11 @@ int main() {
 
   int num; // define variable num as an integer variable
   std::cin >> num; // get integer value from user's keyboard
+
+  int doublenum = num * 2;
+  std::cout << "Double that number is" << doublenum << '\n';
+
+  std::cout << "And triple is " << num * 3 << '\n';
   
   return 0;
 }
