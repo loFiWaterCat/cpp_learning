b0494d0b68074a7eba643d740e7ec79cef35d38e
alan <alan001tk@gmail.com>
2023-01-10 23:07:34 -0800

    Start double program

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cpp_learning
+
+Learning cpp by following the tutorial at https://www.learncpp.com/cpp-tutorial/developing-your-first-program/
---
+++ develop_first_program/double.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Enter an integer: ";
+
+  int num; // define variable num as an integer variable
+  std::cin >> num; // get integer value from user's keyboard
+  
+  return 0;
+}
